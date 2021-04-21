98587b021b1f6ae12ae1c49dec7ee11cd274a3b0
yogurtfinance <yogurtfinance@gmail.com>
2021-04-22 00:24:56 +0300

    Initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,74 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://www.yogurtfinance.com/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://www.yogurtfinance.com/'
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: 'https://www.yogurtfinance.com/nests'
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.yogurtfinance.com/lottery'
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x2efe591e96ad36aa3bb769d486afd713ce2f91b4',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/yogurt-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/yogurt-finance/',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0xd585be33109c1adfa6c0b1edae603eaf1e9bb979',
+      },
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/yogurtfinance/",
+      },
+    ],
+  },
+
+]
+
+export default config
---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x2efe591e96ad36aa3bb769d486afd713ce2f91b4',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xD585be33109c1adfa6c0B1EdaE603EaF1E9Bb979'
+}
+
+export default priceContracts
